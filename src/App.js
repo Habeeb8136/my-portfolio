ac68feeb0e6c1a5b1dad42625abc1153a9b7856a
Habeeb8136 <habeeb041099@gmail.com>
2022-12-06 16:04:23 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
 import Projects from './Components/Projects/Projects';
 
 function App() {
   return (
+    <div className='App'>
+    <BrowserRouter>
+     <Navbar />
+            
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/projects' element ={<Projects />}/>
-      
+
     </Routes>
-    
+    <Footer />
+    </BrowserRouter>
+    </div>
   );
 }
 
