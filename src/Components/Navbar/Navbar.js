a1e61074119a6a100728d02558c97903376f5c20
Habeeb8136 <habeeb041099@gmail.com>
2022-12-07 17:30:46 +0530

    changed order

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,20 +3,20 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <div className='navContainer'>
-          <div className='navHome'><h4>Habeeb Muhammad Arif</h4></div>
+          <div className='navHome'><h4>Home</h4></div>
           <ul className='navLinks'>
-            <li>Projects</li>
             <li>About</li>
+            <li>Projects</li>
             <li>Contact</li>
           </ul>
           
           <ul className='connect'>
-          <li>Git</li>
-          <li>Li</li>
-          <li>Mail</li>
-          <li>ig</li>
-          
+           <li>Git</li>
+           <li>Li</li>
+           <li>Mail</li>
+           <li>ig</li>
           </ul>
+
         </div>
     );
 }
