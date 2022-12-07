a5dbce8d16f9405ae8f56e2883d25c32659a2012
Habeeb8136 <habeeb041099@gmail.com>
2022-12-07 17:29:57 +0530

    new component

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Fotter.css'
+const Footer = () => {
+    return (
+        <div className='footerContainer'>
+            <h1>footer</h1>
+        </div>
+    );
+}
+
+export default Footer;
