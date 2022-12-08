c59a49af3a5e3c51263645d01c11ddd2a1efb1df
Habeeb8136 <habeeb041099@gmail.com>
2022-12-08 15:53:10 +0530

    added popup menu for small screnn devices

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'
+import {FaBars, FaTimes} from 'react-icons/fa'
 const Navbar = () => {
+  const [Clicked, setClicked] = useState(false);
+  const handleClick=()=>{
+    setClicked(!Clicked)
+  }
     return (
         <div className='navContainer'>
           <div className='navHome'><h4>Home</h4></div>
@@ -16,6 +21,26 @@ const Navbar = () => {
            <li>Mail</li>
            <li>ig</li>
           </ul>
+<div className='menuBtn' onClick={handleClick} >
+{Clicked? <FaTimes />:<FaBars />}
+
+
+</div>
+
+<div className={Clicked? 'navSmScreen active':'navSmScreen'}>
+<ul className='navLinksSmScreen'>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+          
+          <ul className='connectsmScreen'>
+           <li>Git</li>
+           <li>Li</li>
+           <li>Mail</li>
+           <li>ig</li>
+          </ul>
+</div>
 
         </div>
     );
