f9604d57e97275c2cb6d1aa1a45f04a754d3fee1
Habeeb8136 <habeeb041099@gmail.com>
2022-12-09 15:18:17 +0530

    footer complete

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './Fotter.css'
+import { AiFillGithub ,AiFillLinkedin ,AiFillMail} from 'react-icons/ai'
+import {ImWhatsapp} from 'react-icons/im'
 const Footer = () => {
     return (
         <div className='footerContainer'>
-            <h1>footer</h1>
+        <div >
+        <h2>Habeeb Muhammad Arif</h2>
+            <h5>Porfolio app build using React</h5>
+        </div>
+            <ul className='connect'>
+           <li><AiFillGithub className='gitLogo'/></li>
+           <li><AiFillLinkedin className='linkedinLogo'/></li>
+           <li><AiFillMail className='mailLogo'/></li>
+           <li><ImWhatsapp className='whatsappLogo'/></li>
+          </ul>
         </div>
     );
 }
