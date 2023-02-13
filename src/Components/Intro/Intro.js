3b3f6b44bd3cbc3529c84a891b6112eec19cb8a9
Habeeb8136 <habeeb041099@gmail.com>
2023-02-13 20:50:29 +0530

    apdated cv

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,4 +1,4 @@
-import CV from '../../Assets/Habeeb muhammad arif CV (2).pdf'
+import CV from '../../Assets/Habeeb muhammad arif Resume (5).pdf'
 import React from 'react';
 import './Intro.css'
 const Intro = () => {
